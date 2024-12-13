0c7aca2c0d98587b559ecba9c9211ae97498e489
Jayvan <jayvanramos23@gmail.com>
2024-12-13 12:05:08 -0400

    added 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,37 +1,46 @@
 #include "sort.h"
 #include <stdio.h>
 
+/**
+ * insertion_sort_list-Sorts adoubly linked list of integers in ascending order
+ * @list: Pointer to the head of the doubly linked list
+ *
+ * DescriptionThis function sorts a doubly linked list using the insertion sort
+ * algorithm. It compares the nodes in the list and moves them into their
+ * correct positions in the sorted part of the list. After each swap, the
+ * list is printed.
+ */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *insert, *temp;
+	listint_t *current, *insert, *temp;
 
-    if (!list || !*list || !(*list)->next)
-        return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
-    current = (*list)->next;
-    while (current)
-    {
-        insert = current;
-        current = current->next;
+	current = (*list)->next;
+	while (current)
+	{
+		insert = current;
+		current = current->next;
 
-        while (insert->prev && insert->n < insert->prev->n)
-        {
-            temp = insert->prev;
-            if (temp->prev)
-                temp->prev->next = insert;
+		while (insert->prev && insert->n < insert->prev->n)
+		{
+			temp = insert->prev;
+			if (temp->prev)
+				temp->prev->next = insert;
 
-            insert->prev = temp->prev;
-            temp->next = insert->next;
-            if (insert->next)
-                insert->next->prev = temp;
+			insert->prev = temp->prev;
+			temp->next = insert->next;
+			if (insert->next)
+				insert->next->prev = temp;
 
-            insert->next = temp;
-            temp->prev = insert;
+			insert->next = temp;
+			temp->prev = insert;
 
-            if (!insert->prev)
-                *list = insert;
+			if (!insert->prev)
+				*list = insert;
 
-            print_list(*list);
-        }
-    }
+			print_list(*list);
+		}
+	}
 }
